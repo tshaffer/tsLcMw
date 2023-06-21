f8b59993b4d1ca7006df35ea18bd8625bf1c7296
Ted Shaffer <shaffer.family@gmail.com>
2023-06-21 06:33:29 -0700

    Tried a new prompt to see what openAI would say.

--- src/basic.ts
+++ src/basic.ts
@@ -4,7 +4,8 @@ export const run = async () => {
   // temperature controls how random/creative the response is. It ranges from 0(deterministic) to 1(max creativity)
   const model = new OpenAI({ temperature: 0.1 });
 
-  const res = await model.call("What is the capital city of France?");
+  // const res = await model.call("What is the capital city of France?");
+  const res = await model.call("What would be a good meal to prepare that is similar to Spaghetti Carbonara?");
   console.log({ res });
   //output: "Paris"
 };
